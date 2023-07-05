1570f87be6cb87d8d48aaf37191e53939140a742
audreyvolle <48231908+audreyvolle@users.noreply.github.com>
2023-07-05 00:30:17 -0500

    puppeteer base

--- AcademicNav/src/pages/welcome/welcome.tsx
+++ AcademicNav/src/pages/welcome/welcome.tsx
@@ -7,23 +7,56 @@
 
     welcome service will use our web scraping choice. The inputted major should be sent and used for scraping once next is clicked.
 */
-import {Routes, Route, useNavigate} from 'react-router-dom';
-import './welcome.scss'
-import  NewView  from './new-view-input/new-view-input';
+import { useState } from 'react';
+import { Routes, Route, useNavigate } from 'react-router-dom';
+import './welcome.scss';
+import NewView from './new-view-input/new-view-input';
+import puppeteer from 'puppeteer';
 
 function Welcome() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
+  const [selectedValue, setSelectedValue] = useState('');
 
-  const navigateToNiewView = () => {
-    navigate('/new-view');
+  const handleDropdownChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelectedValue(event.target.value);
   };
 
+  const buildUrl = () => {
+    // Implement your logic to build the URL based on the selected value
+    // For simplicity, let's assume it's a simple concatenation with a base URL
+    const baseUrl = 'https://example.com/';
+    const url = baseUrl + selectedValue;
+    return url;
+  };
+
+  const runWebScraping = async () => {
+    const url = buildUrl();
+    // Implement your web scraping logic using Puppeteer or other libraries
+    // based on the URL obtained from buildUrl()
+    // For example:
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(url);
+    await page.screenshot({ path: 'screenshot.png' });
+    await browser.close();
+  };
+
+  const navigateToNewView = () => {
+    runWebScraping();
+    navigate('/new-view');
+  };
 
   return (
     <div>
       <div>
         <hr />
-        <button onClick={navigateToNiewView}>Input Class Info</button>
+        <select value={selectedValue} onChange={handleDropdownChange}>
+          <option value="">Select an option</option>
+          <option value="computer-science-ba">Computer Science BA</option>
+          <option value="computer-science-bs">Computer Science BS</option>
+          <option value="public-health">Public Health</option>
+        </select>
+        <button onClick={navigateToNewView}>Input Class Info</button>
 
         <Routes>
           <Route path="/new-view" element={<NewView />} />
@@ -32,5 +65,5 @@ function Welcome() {
     </div>
   );
 }
-  
-  export default Welcome;
+
+export default Welcome;
