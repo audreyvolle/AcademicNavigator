181c570cc4fca4d8a1ecbe853ab13f3ba155f97a
Darin <98626579+DarinRagan@users.noreply.github.com>
2023-09-04 19:21:08 -0500

    Format of Block View
    Initial Block View formatting done. Using the same color coding as the Graph View right now and taking class data from test files.
    
    Will be working on a drag & drop handler to move classes between semesters. It should be similar to how we handle the class sidebar.

--- AcademicNav/src/Providers/UserProv.tsx
+++ AcademicNav/src/Providers/UserProv.tsx
@@ -8,13 +8,13 @@ import ClassList from "../pages/class-view/side-bar/class-list/class-list";
 //Add everything in exportValue
 //Set up the initial state
 interface ClassList {
-  id: string,
-  title: string,
-  credits: number,
-  prerequisites: Array<string>,
-  prerequisitesTaken: Array<string>,
-  isReadyToTake: boolean,
-  taken: boolean
+    id: string,
+    title: string,
+    credits: number,
+    prerequisites: Array<string>,
+    prerequisitesTaken: Array<string>,
+    isReadyToTake: boolean,
+    taken: boolean
 }
 const classArray: ClassList[] = classData as ClassList[];
 
@@ -22,7 +22,7 @@ export interface exportedValue {
     selectedValue: string;
     setSelectedValue: React.Dispatch<React.SetStateAction<string>>;
     handleDropdownChange: (event: React.ChangeEvent<HTMLSelectElement>) => void;
-    isMainViewVisible:boolean;
+    isMainViewVisible: boolean;
     setIsMainViewVisible: React.Dispatch<React.SetStateAction<boolean>>;
     selectedClasses: string[];
     setSelectedClasses: React.Dispatch<React.SetStateAction<string[]>>;
@@ -33,24 +33,24 @@ export interface exportedValue {
     setClassArray: React.Dispatch<React.SetStateAction<ClassList[]>>;
 }
 
-const initialState:exportedValue = {
+const initialState: exportedValue = {
     selectedValue: '',
     isMainViewVisible: false,
     selectedClasses: [],
     classArray: [],
-    setSelectedClasses: () => {},
-    setIsMainViewVisible: () => {},
-    setSelectedValue: () => {},
-    handleDropdownChange: () => {},
-    handleCheckboxChange: () => {},
-    handleSkipClick: () => {},
-    handleContinueClick: () => {},
-    setClassArray: () => {}
+    setSelectedClasses: () => { },
+    setIsMainViewVisible: () => { },
+    setSelectedValue: () => { },
+    handleDropdownChange: () => { },
+    handleCheckboxChange: () => { },
+    handleSkipClick: () => { },
+    handleContinueClick: () => { },
+    setClassArray: () => { }
 };
 
 export const UserInfoContext = createContext<exportedValue>(initialState);
 
-export const UserProvider = ({children}: { children: ReactNode}) => {
+export const UserProvider = ({ children }: { children: ReactNode }) => {
     const [selectedValue, setSelectedValue] = useState<string>("");
     const [isMainViewVisible, setIsMainViewVisible] = useState<boolean>(false);
     const [selectedClasses, setSelectedClasses] = useState<string[]>([]);
@@ -59,56 +59,56 @@ export const UserProvider = ({children}: { children: ReactNode}) => {
     const handleDropdownChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
         const selectedValue = event.target.value;
         setSelectedValue(selectedValue);
-    
+
         // Get the corresponding major abbreviation from the key
         const selectedMajorAbbreviation = majorAbbreviationKey[selectedValue];
         const majorFullName = majorFullKey[selectedValue];
         if (selectedMajorAbbreviation) {
-          buildUrlForMajor(selectedMajorAbbreviation, majorFullName);
+            buildUrlForMajor(selectedMajorAbbreviation, majorFullName);
         }
         setClassArray(classData as ClassList[]);
-      };
-    
-    function buildUrlForMajor(majorAbbreviation: string, majorFull: string) {
-    const baseUrl = 'https://www.siue.edu/academics/undergraduate/courses/index.shtml';
-    const reqBaseURL = 'https://www.siue.edu/academics/undergraduate/degrees-and-programs/';
-    const reqTail = '/degree-requirements.shtml';
-    const queryString = `?subject=${majorAbbreviation}`;
-    const reqQueryString = `?${majorFull}`;
-    const classListURL = baseUrl + queryString;
-    const requirementsURL = reqBaseURL + reqQueryString + reqTail;
+    };
 
-    //scrapeCourseList(classListURL);
-    //scrapeDegreeRequirements(requirementsURL);
-    //printToJson(courseList);
+    function buildUrlForMajor(majorAbbreviation: string, majorFull: string) {
+        const baseUrl = 'https://www.siue.edu/academics/undergraduate/courses/index.shtml';
+        const reqBaseURL = 'https://www.siue.edu/academics/undergraduate/degrees-and-programs/';
+        const reqTail = '/degree-requirements.shtml';
+        const queryString = `?subject=${majorAbbreviation}`;
+        const reqQueryString = `?${majorFull}`;
+        const classListURL = baseUrl + queryString;
+        const requirementsURL = reqBaseURL + reqQueryString + reqTail;
+
+        //scrapeCourseList(classListURL);
+        //scrapeDegreeRequirements(requirementsURL);
+        //printToJson(courseList);
     }
 
     const handleCheckboxChange = (className: string) => {
         if (selectedClasses.includes(className)) {
-          setSelectedClasses(selectedClasses.filter((c) => c !== className));
+            setSelectedClasses(selectedClasses.filter((c) => c !== className));
         } else {
-          setSelectedClasses([...selectedClasses, className]);
-          const updatedClasses = classArray.map(course => {
-            if (course.id === className) {
-              return {
-                ...course,
-                taken: true
-              };
-            }
-            return course;
-          });
-          setClassArray(updatedClasses);
+            setSelectedClasses([...selectedClasses, className]);
+            const updatedClasses = classArray.map(course => {
+                if (course.id === className) {
+                    return {
+                        ...course,
+                        taken: true
+                    };
+                }
+                return course;
+            });
+            setClassArray(updatedClasses);
         }
-      };
-    
-      const handleSkipClick = () => {
+    };
+
+    const handleSkipClick = () => {
         setIsMainViewVisible(true);
-      };
-    
-      const handleContinueClick = () => {
+    };
+
+    const handleContinueClick = () => {
         setIsMainViewVisible(true);
-      };
-    
+    };
+
     const value = {
         //here is the value we should export
         selectedValue,
--- AcademicNav/src/pages/class-view/main-view/block-view/block-view.tsx
+++ AcademicNav/src/pages/class-view/main-view/block-view/block-view.tsx
@@ -1,22 +1,171 @@
 import './block-view.scss'
-import { ReactFlow } from 'reactflow';
+import { Node, ReactFlow, useNodesState } from 'reactflow';
+import React, { useState, useCallback } from 'react'; //FROM GRAPH VIEW
+//import { useUser } from '../../../../Providers/UserProv';
+import testData from '../../../../data/scraped/test2.json';
 import 'reactflow/dist/style.css';
 
-const initialNodes = [
-  { id: '1', position: { x: 0, y: 0 }, data: { label: '1' } },
-  { id: '2', position: { x: 0, y: 100 }, data: { label: '2' } },
-];
-const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }];
 
-function BlockView() {
-  return (
-    <div>
-        <p>block view</p>
-        <div style={{ width: '66vw', height: '80vh' }}>
-          <ReactFlow nodes={initialNodes} edges={initialEdges} />
+//arrays
+const semesters: string | string[] = [];
+
+//colors
+const takenColor = 'rgba(178,255,102,1)';
+const readyColor = 'rgb(255,255,255)';
+const unavailableColor = 'rgba(255,153,153,1)';
+let setColor = 'rgb(255,255,255)';
+const addSemesterColor = 'rgb(128,128,128)'
+
+
+const BlockView = () => {
+
+    let groupCount = 0;
+    let parentID = 0;
+    let col = 0;
+
+    const [nodes, setNodes, onNodesChange] = useNodesState([]);
+
+    //get class data
+    //const { classArray } = useUser(); //Use this when incorporating UserProv
+    const classArray = testData;
+
+    //populate the semesters array
+    classArray.forEach(function (value) {
+        if (!semesters.includes(value.semester) && value.semester != null) {
+            semesters.push(value.semester);
+        }
+    })
+
+    //sort semesters by year/season
+    customSort(semesters)
+
+    const semesterClassCount = new Array(semesters.length).fill(0);
+
+    //create nodes for each semester
+    semesters.forEach(function (value) {
+        nodes.push({
+            id: value,
+            data: { label: value },
+            position: { x: 25 + (col * 425), y: 25 + (groupCount * 275) },
+            className: 'light',
+            style: { backgroundColor: 'rgba(225,225,225,0)', width: 400, height: 250, fontSize: 20 },
+            selectable: false,
+            connectable: false
+        })
+        if (col == 0) {
+            col++
+        }
+        else {
+            groupCount++
+            col--
+        }
+    })
+
+    //add a button node to add additional semesters
+    nodes.push({
+        id: 'addSemester',
+        data: { label: 'Add Semester' },
+        position: { x: 25 + (col * 425), y: 25 + (groupCount * 275) },
+        className: 'light',
+        style: { backgroundColor: addSemesterColor, width: 400, height: 250, fontSize: 30, verticalAlign: 'middle' },
+        selectable: false,
+        connectable: false
+    })
+
+    //Add classes to the display
+    classArray.forEach(function (value) {
+        //set color based off class status
+        if (value.taken) {
+            setColor = takenColor
+        }
+        else if (value.isReadyToTake) {
+            setColor = readyColor
+        }
+        else {
+            setColor = unavailableColor
+        }
+
+        parentID = semesters.findIndex(item => item === value.semester)
+        if (value.semester != null) {
+            nodes.push(
+                {
+                    id: value.id,
+                    position: { x: 25, y: 20 + (semesterClassCount[parentID] + 1) * 30 },
+                    data: { label: value.id + ": " + value.title },
+                    style: { backgroundColor: setColor, width: 350 },
+                    parentNode: value.semester,
+                    extent: 'parent',
+                    expandParent: true,
+                    selectable: true,
+                    draggable: true,
+                    dragging: false,
+                    focusable: true,
+                    connectable: false
+                })
+        }
+        semesterClassCount[parentID]++
+    })
+
+    const onClick = useCallback((event: React.MouseEvent, node: Node) => {
+        event.preventDefault()
+        if (node.id === 'addSemester') {
+            let semOutput = "";
+            const lastindex = semesters[semesters.length - 1]
+
+            const year = parseInt(lastindex.match(/\d+/)[0])
+            const isSpring = lastindex.includes("Spring")
+            //const groupposition = groupcount * groupspacing
+
+            if (isSpring) {
+                semOutput = "Fall " + year
+            }
+            else {
+                semOutput = "Spring " + (year + 1)
+            }
+            semesters.push(semOutput)
+            nodes.push({
+                id: semOutput,
+                data: { label: semOutput },
+                position: { x: 25 + (col * 425), y: 25 + (groupCount * 275) },
+                className: 'light',
+                style: { backgroundColor: 'rgba(225,225,225,0)', width: 400, height: 250, fontSize: 20 },
+                selectable: false,
+                connectable: false
+            })
+            groupCount++
+            console.log(nodes)
+            console.log(groupCount)
+        }
+    }, [groupCount, nodes, col])
+
+    return (
+        <div>
+            <div style={{ width: '66vw', height: '80vh' }}>
+                <ReactFlow
+                    nodes={nodes}
+                    onNodesChange={onNodesChange}
+                    panOnScroll={true}
+                    panOnDrag={false}
+                    zoomOnDoubleClick={false}
+                    onNodeClick={onClick}
+                />
+            </div>
         </div>
-    </div>
-  );
+    );
+}
+
+function customSort(arr: string[]): string[] {
+    return arr.sort((a, b) => {
+        const aYear = parseInt(a.match(/\d+/)[0])
+        const bYear = parseInt(b.match(/\d+/)[0])
+        const aSeason = a.includes("Spring") ? 0 : 1
+        const bSeason = b.includes("Spring") ? 0 : 1
+
+        if (aYear !== bYear) {
+            return aYear - bYear
+        }
+        return aSeason - bSeason
+    });
 }
   
 export default BlockView;
