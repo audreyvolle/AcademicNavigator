94af313f9fa4ce20f8b214212738c1179efb136a
audreyvolle <48231908+audreyvolle@users.noreply.github.com>
2023-09-20 10:08:24 -0500

    critical path for cs ba and bs

--- AcademicNav/src/pages/class-view/main-view/critical-path/critical-path.tsx
+++ AcademicNav/src/pages/class-view/main-view/critical-path/critical-path.tsx
@@ -5,8 +5,8 @@ import computerScienceBA from '/public/images/critical-path-images/computer-scie
 import computerScienceBS from '/public/images/critical-path-images/computer-science-bs.svg';
 import publicHealth from '/public/images/critical-path-images/public-health.svg';
 import test from '/public/images/critical-path-images/test.svg';
-// Add more SVG files as needed
 
+// Add more SVG files as needed
 const svgFiles = {
   'computer-science-ba': computerScienceBA,
   'computer-science-bs': computerScienceBS,
@@ -14,13 +14,22 @@ const svgFiles = {
   'test': test
 };
 
+const fullMajorNames = {
+  'computer-science-ba': "Computer Science BA",
+  'computer-science-bs': "Computer Science BS",
+  'public-health': "Public Health",
+  'test': "Test"
+};
+
 const CriticalPath = () => {
   const { selectedValue } = useUser();
-    console.log(selectedValue);
+  console.log(selectedValue);
   const selectedSVG = svgFiles[selectedValue as keyof typeof svgFiles];
+  const major = fullMajorNames[selectedValue as keyof typeof fullMajorNames];
 
   return (
     <div className='critical-path-container'>
+      <h1 className='critical-path-heading'>{major} Critical Path</h1>
       {selectedSVG && (
         <img className='critical-path-img' src={selectedSVG} alt={`SVG for ${selectedValue}`} />
       )}
