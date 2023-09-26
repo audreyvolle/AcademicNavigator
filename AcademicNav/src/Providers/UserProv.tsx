3692b3ddad3cfdcb5be112eb3d51e5663e6c46b4
MissileMann <misael.escoto043@gmail.com>
2023-09-25 20:14:52 -0500

    Updated graph view for prerequisite changes and minor tweaks

--- AcademicNav/src/Providers/UserProv.tsx
+++ AcademicNav/src/Providers/UserProv.tsx
@@ -7,7 +7,7 @@ import { useData } from "./DataProv";
 //Add everything in exportValue
 //Set up the initial state
 
-type PrerequisiteType = {
+export type PrerequisiteType = {
   id: string;
   Grade: string;
   concurrency: boolean;
