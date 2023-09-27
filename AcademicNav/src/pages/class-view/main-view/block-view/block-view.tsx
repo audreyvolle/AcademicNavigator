a0c7120901d7933b1a702e7e827354efa3ffcd40
Darin <98626579+DarinRagan@users.noreply.github.com>
2023-09-26 20:08:10 -0500

    Fixed Block View
    Restructured the block view code to mirror the Graph View. Left in the methods lastSemester() and fillSemesters() just in case they become useful later on.

--- AcademicNav/src/pages/class-view/main-view/block-view/block-view.tsx
+++ AcademicNav/src/pages/class-view/main-view/block-view/block-view.tsx
@@ -11,9 +11,15 @@ import './block-view.scss';
 import { useUser } from '../../../../Providers/UserProv';
 
 
+//formatting
 
-//arrays
-const coreSemesters: string | string[] = [];
+const spacing = 25;
+const horizontalSpacing = 425;
+const verticalSpacing = 275;
+
+const semWidth = 400;
+const semHeight = 250;
+const classWidth = 350;
 
 //colors
 const takenColor = 'rgba(178,255,102,1)';
@@ -22,128 +28,150 @@ const unavailableColor = 'rgba(255,153,153,1)';
 let setColor = 'rgb(255,255,255)';
 const addSemesterColor = 'rgb(128,128,128)'
 
+//arrays
+const semesters: string | string[] = [];
+let semesterClassCount: number[] = [];
+
+//counts
+let groupCount = 0;
+let parentID = 0;
+let col = 0;
+const warningMessageDuration = 3500;
 
 
 const BlockView = () => {
-    const { classArray, setClassArray } = useUser();
+    const { classArray, setClassArray, currentSemester } = useUser();
     const reactFlowWrapper = useRef(null);
     const [reactFlowInstance, setReactFlowInstance] = useState<ReactFlowInstance | null>(null);
     const [nodes, setNodes, onNodesChange] = useNodesState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [showWarning, setShowWarning] = useState(false);
+    const [warningMessage, setWarningMessage] = useState('');
 
-
-    let groupCount = 0;
-    let parentID = 0;
-    let col = 0;
 
     useEffect(() => {
-        if (reactFlowInstance) {
-            // reactFlowInstance is available, perform initialization here
-            console.log('Component is initialized');
+
+        //cleans up the empty semesters at the end
+        if (semesters.length > 0) {
+            //console.log(semesters)
+            let emptySemesters = -1;
+            for (let i = semesters.length; i > 0; i--) {
+                if (semesterClassCount[i - 1] > 0 || (i === 1 && emptySemesters === - 1)) {
+                    emptySemesters = i
+                    break
+                }
+            }
+            //console.log(emptySemesters)
+            if (emptySemesters != -1) {
+                //console.log("slicing")
+                const slicedSemesters = semesters.slice(0, emptySemesters)
+                semesters.splice(0, semesters.length)
+                //console.log(slicedSemesters)
+                slicedSemesters.forEach(function (value) {
+                    semesters.push(value)
+                })
+            }
+            //console.log(semesters)
         }
 
-    }, [reactFlowInstance, classArray]);
+        //set up the arrays for the display formatting
+        groupCount = 0;
+        col = 0;
 
-
-
-
-
-
-
-
-    //populate the semesters array
-    classArray.forEach(function (value) {
-
-        if (!coreSemesters.includes(value.semester) && value.semester != null && value.semester != "" && !value.taken) {
-            coreSemesters.push(value.semester);
+        /*
+        if (!semesters.includes(currentSemester)) {
+            semesters.push(currentSemester);
         }
-    })
+        */
 
 
-    const semesters: string[] = fillSemesters(lastSemester(coreSemesters)); //Fills semesters with every semester from now until the last registered class
-    const semesterClassCount = new Array(semesters.length).fill(0);
+        classArray.forEach(function (value) {
+            if (!semesters.includes(value.semester) && value.semester != null && value.semester != "") {
+                semesters.push(value.semester);
+            }
+        })
+        customSort(semesters)
+        semesterClassCount = new Array(semesters.length).fill(0);
 
-    //create nodes for each semester
-    semesters.forEach(function (value) {
 
-        nodes.push({
-            id: value,
-            data: { label: value },
-            position: { x: 25 + (col * 425), y: 25 + (groupCount * 275) },
-            className: 'light',
-            style: { backgroundColor: 'rgba(225,225,225,0)', width: 400, height: 250, fontSize: 20 },
-            selectable: false,
-            connectable: false
+        //make a new Block for each semester
+        console.log('adding the semesters as groups')
+        semesters.forEach(function (value) {
+            nodes.push({
+                id: value,
+                data: { label: value },
+                position: { x: spacing + (col * horizontalSpacing), y: spacing + (groupCount * verticalSpacing) },
+                className: 'light',
+                style: { backgroundColor: 'rgba(225,225,225,0)', width: semWidth, height: semHeight, fontSize: 20 },
+                selectable: false,
+                connectable: false,
+                draggable: false
+            })
+            if (col == 0) {
+                col++
+            }
+            else {
+                col--
+                groupCount++
+            }
+
+            console.log('Semester added: ' + value)
         })
 
-        if (col == 0) {
-            col++
-        }
-        else {
-            groupCount++
-            col--
-        }
-    })
+        //Add an extra block for the add semester button
+        console.log('adding semester button')
+        nodes.push({
+            id: 'addSemester',
+            data: { label: 'Add Semester' },
+            position: { x: spacing + (col * horizontalSpacing), y: spacing + (groupCount * verticalSpacing) },
+            className: 'light',
+            style: { backgroundColor: addSemesterColor, width: semWidth, height: semHeight, fontSize: 30, verticalAlign: 'middle' },
+            selectable: false,
+            connectable: false,
+            draggable: false
+        })
+        console.log('semester button added')
 
-    //add a button node to add additional semesters
-    nodes.push({
-        id: 'addSemester',
-        data: { label: 'Add Semester' },
-        position: { x: 25 + (col * 425), y: 25 + (groupCount * 275) },
-        className: 'light',
-        style: { backgroundColor: addSemesterColor, width: 400, height: 250, fontSize: 30, verticalAlign: 'middle' },
-        selectable: false,
-        connectable: false
-    })
-
-    //Add classes to the display
-    classArray.forEach(function (value) {
-        //set color based off class status
-        if (value.taken) {
-            setColor = takenColor
-        }
-        else if (value.isReadyToTake) {
-            setColor = readyColor
-        }
-        else {
-            setColor = unavailableColor
-        }
-        
-        parentID = semesters.findIndex(item => item === value.semester)
-        if (value.semester != null && value.semester != "") {
-            //if it has untaken prereqs, make it red
-            if (value.prerequisites && value.prerequisites.length > value.prerequisitesTaken.length) {
+        //Add classes to the display
+        classArray.forEach(function (value) {
+            //set color based off class status
+            if (value.taken) {
+                setColor = takenColor
+            }
+            else if (value.isReadyToTake) {
+                setColor = readyColor
+            }
+            else {
                 setColor = unavailableColor
             }
 
-            if (!value.taken) {
-                nodes.push(
-                    {
-                        id: value.id,
-                        position: { x: 25, y: 20 + (semesterClassCount[parentID] + 1) * 30 },
-                        data: { label: value.id + ": " + value.title },
-                        style: { backgroundColor: setColor, width: 350 },
-                        parentNode: value.semester,
-                        extent: 'parent',
-                        expandParent: true,
-                        selectable: true,
-                        draggable: true,
-                        dragging: false,
-                        focusable: true,
-                        connectable: false
-                    })
-
-                semesterClassCount[parentID]++
+            parentID = semesters.findIndex(item => item === value.semester)
+            if (value.semester != null && value.semester != "") {
+                
+                if (!value.taken) {
+                    nodes.push(
+                        {
+                            id: value.id,
+                            position: { x: spacing, y: 20 + (semesterClassCount[parentID] + 1) * 30 },
+                            data: { label: value.id + ": " + value.title },
+                            style: { backgroundColor: setColor, width: classWidth },
+                            parentNode: value.semester,
+                            expandParent: true,
+                            selectable: false,
+                            draggable: false,
+                            dragging: false,
+                            focusable: true,
+                            connectable: false
+                        })
+                    semesterClassCount[parentID]++
+                }
             }
-        }
-    })
+        })
 
+        setColor = readyColor
+        setIsLoading(false)
 
-
-
-
-
-
-
+    }, []);
 
 
     //Click Event Handler
@@ -172,7 +200,6 @@ const BlockView = () => {
                 semOutput = "Spring " + (year + 1)
             }
             semesters.push(semOutput)
-            coreSemesters.push(semOutput)
 
             nodes.push({
                 id: semOutput,
@@ -181,7 +208,8 @@ const BlockView = () => {
                 className: 'light',
                 style: { backgroundColor: 'rgba(225,225,225,0)', width: 400, height: 250, fontSize: 20 },
                 selectable: false,
-                connectable: false
+                connectable: false,
+                draggable: false
             })
             if (col == 0) {
                 col++
@@ -190,29 +218,29 @@ const BlockView = () => {
                 groupCount++
                 col--
             }
-            console.log(nodes)
-            console.log(groupCount)
+            semesterClassCount.push(0);
+
+            const nodeToUpdate = nodes.findIndex((nnode) => nnode.id === node.id)
+
+            if (nodeToUpdate !== -1) {
+                const updatedNodes = [...nodes]
+                updatedNodes[nodeToUpdate] = {
+                    ...updatedNodes[nodeToUpdate],
+                    position: { x: 25 + (col * 425), y: 25 + (groupCount * 275) },
+                };
+                setNodes(updatedNodes);
+            }
+
+            //console.log(nodes)
+            //console.log(groupCount)
         }
 
-        /*
-        else {
-            if (node.parentNode) {
-                setHoverSem(node.parentNode)
-            }
-            else {
-                setHoverSem(node.id);
-            }
-        }
-
-        */
-
-    }, [groupCount, nodes, col, semesters])
+    }, [nodes, setNodes])
 
     //Drag Event Handler
     const onDragOver = useCallback((event: any) => {
         event.preventDefault();
         event.dataTransfer.dropEffect = 'move';
-
     }, []);
 
     //Drop Event Handler
@@ -238,12 +266,12 @@ const BlockView = () => {
 
                     //loop through all nodes
                     for (const element of nodes) {
-                        const parentId = semesters.findIndex((semester) => semester === element.id);
+                        parentID = semesters.findIndex((semester) => semester === element.id);
 
                         //if the target node's ID is a semester with a block and the cursor is over it
                         if (
                             element.id != null &&
-                            parentId != -1 &&
+                            parentID != -1 &&
                             position.x >= element.position.x &&
                             position.x <= element.position.x + element.width &&
                             position.y >= element.position.y &&
@@ -254,16 +282,32 @@ const BlockView = () => {
 
 
                             if (classToMove) {
+
+                                const newNode = {
+                                    id: classToMove.id,
+                                    position: { x: spacing, y: 20 + (semesterClassCount[parentID] + 1) * 30 },
+                                    data: { label: classToMove.id + ": " + classToMove.title },
+                                    style: { backgroundColor: setColor, width: classWidth },
+                                    parentNode: element.id,
+                                    expandParent: true,
+                                    selectable: false,
+                                    draggable: false,
+                                    dragging: false,
+                                    focusable: true,
+                                    connectable: false
+                                }
+
                                 const updatedClassArray = classArray.map((classItem) => classItem === classToMove ? {
-                                    ...classItem, semester: element.id
+                                    ...classItem, taken: true, semester: element.id
                                 } : classItem);
 
+                                semesterClassCount[parentID]++
                                 setClassArray(updatedClassArray);
                                 console.log(classArray);
 
+                                setNodes((nds) => nds.concat(newNode));
                             }
 
-                            //setNodes(nodes);
 
                         } //end of if
                     }
@@ -272,36 +316,104 @@ const BlockView = () => {
 
             }
         },
-        [reactFlowInstance, setClassArray, classArray, nodes, semesters, setNodes]
+        [reactFlowInstance, setClassArray, classArray, nodes, setNodes]
     );
 
-    //sets hoverSem to the id of the semester block being moused over
-    /*
-    const mouseOver = useCallback((event: React.MouseEvent, node: Node) => {
+    //used for node deletion
+    const onNodeDoubleClick = useCallback((event: React.MouseEvent, node: Node) => { //use for deleting a class node or semester node
         event.preventDefault()
-
-        if (node.parentNode) {
-            setHoverSem(node.parentNode)
+        if (node.id === 'addSemester') {
+            console.log("add semester double clicked, how did you do this...")
+            return
         }
-        else {
-            setHoverSem(node.id);
+        else if (semesters.findIndex((semester) => semester === node.id) != -1) { //if its a semester node
+            const semesterLastIndex = semesters.length - 1;
+
+            //if it's the last semester in the array
+            if (semesters.findIndex((semester) => semester === node.id) === semesterLastIndex) {
+
+                //if last semester in the array is empty (no classes)
+                if (semesterClassCount[semesterLastIndex] === 0) {
+                    console.log("last semester double clicked")
+                    const semesterToRemove = semesters[semesterLastIndex]
+                    if (col == 0) {
+                        col++
+                        groupCount--
+                    }
+                    else {
+                        col--
+                    }
+                    semesters.pop()
+                    semesterClassCount.pop()
+                    console.log(nodes)
+                    nodes.splice(nodes.findIndex((node) => node.id === semesterToRemove), 1)
+                    const nodeToUpdate = nodes.findIndex((nnode) => nnode.id === 'addSemester')
+
+                    if (nodeToUpdate !== -1) {
+                        const updatedNodes = [...nodes]
+                        updatedNodes[nodeToUpdate] = {
+                            ...updatedNodes[nodeToUpdate],
+                            position: { x: spacing + (col * horizontalSpacing), y: spacing + (groupCount * verticalSpacing) },
+                        };
+                        setNodes(updatedNodes);
+                    }
+                    //setNodes(nodes)
+                    return
+                } else {
+                    triggerWarning("Cannot Remove a Semester that has classes")
+                }
+            } else {
+                triggerWarning("Can only remove the last semester (Temp)")
+                console.log("non last semester double clicked")
+                return
+            }
         }
+        else { //it's a class node
+            console.log("class node doubleclicked")
+            const classInfo = classArray.find((element) => element.id === node.id)
+            const semesterId = semesters.findIndex((semester) => semester === classInfo.semester)
 
-    }, [setHoverSem])
-    
+            semesterClassCount[semesterId]--
+            const updatedNodes = nodes.filter((element) => element.id !== node.id);
 
-    const nodeClick = useCallback((node: Node) => {
-        //event.preventDefault()
+            //condense the remaining class nodes 
+            let x = 0
+            updatedNodes.forEach((node) => {
+                if (node.parentNode === classInfo.semester) {
+                    x++
 
-        if (node.parentNode) {
-            setHoverSem(node.parentNode)
+                    node.position = { x: spacing, y: 20 + x * 30 }
+                }
+            })
+
+            setNodes(updatedNodes)
+
+
+            const updatedClassArray = classArray.map((classItem) =>
+                classItem === classInfo ? { ...classItem, taken: false, semester: "" } : classItem //Update the class array
+            );
+            setClassArray(updatedClassArray);
+
+            return
         }
-        else {
-            setHoverSem(node.id);
-        }
+    }, [classArray, nodes, setClassArray, setNodes]);
 
-    }, [setHoverSem])
-    */
+    if (isLoading) {
+        return <div>Loading...</div>;
+    }
+
+
+    const triggerWarning = (message: React.SetStateAction<string>) => { //used to alert the user of a warning or some error
+        setShowWarning(false);
+        console.log('Triggering warning...');
+        setWarningMessage(message);
+        setShowWarning(true);
+
+        setTimeout(() => {
+            setShowWarning(false);
+            console.log('Disabling warning...');
+        }, warningMessageDuration);
+    };
 
     return (
         <div className="dndflow">
@@ -315,12 +427,18 @@ const BlockView = () => {
                         zoomOnDoubleClick={false}
                         onInit={setReactFlowInstance}
                         onNodeClick={onClick}
+                        onNodeDoubleClick={onNodeDoubleClick}
                         onDrop={onDrop}
                         onDragOver={onDragOver}
                         fitView
                     />
                 </div>
                 <SideBar />
+                {
+                    <div className={`warning-box ${showWarning ? 'show' : 'hide'}`} >
+                        <div className="message">{warningMessage}</div>
+                    </div>
+                }
             </ReactFlowProvider>
         </div>
     );
